969ad42211054c181d6c82c9e5df6df078f9ed16
Joaquin <axxyss@gmail.com>
2026-06-05 16:54:34 +0200

    Feature: restablecimiento de contraseña con traducciones EN y FR

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 # View
 from users import views
 
@@ -28,5 +29,36 @@ urlpatterns = [
         route='activar/<uuid:token>/',
         view=views.ActivateAccountView.as_view(),
         name='activate'
+    ),
+
+    path(
+        route='password_reset/',
+        view=auth_views.PasswordResetView.as_view(
+            template_name='users/password_reset.html',
+            email_template_name='users/password_reset_email.txt',
+            subject_template_name='users/password_reset_subject.txt',
+        ),
+        name='password_reset'
+    ),
+    path(
+        route='password_reset/done/',
+        view=auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        route='password_reset/<uidb64>/<token>/',
+        view=auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        route='password_reset/complete/',
+        view=auth_views.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
     ),
 ]
