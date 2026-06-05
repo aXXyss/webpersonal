6697d55df1f7578bfbeb6b2f21c95ab3fe7621f4
Joaquin <axxyss@gmail.com>
2026-06-05 16:35:48 +0200

    Fix: saltos de línea en email de activación

--- users/forms.py
+++ users/forms.py
@@ -62,9 +62,12 @@ class SignupForm(forms.Form):
 
         try:
             activation_url = f"{settings.SITE_URL}/{language}/activar/{profile.activation_token}/"
+            msg_template = str(_('Hola %(username)s,\n\nGracias por registrarte en aXXyss Soluciones.\n\nPara activar tu cuenta haz clic en el siguiente enlace:\n\n%(url)s\n\nSi no te has registrado, ignora este mensaje.\n\nUn saludo,\nJoaquin Denis\naXXyss Soluciones\nhttps://axxyss.com\n'))
+            message = msg_template.replace('\\n', '\n') % {'username': user.username, 'url': activation_url}
+
             send_mail(
                 subject=str(_('Activa tu cuenta en aXXyss Soluciones')),
-                message=str(_('Hola %(username)s,\n\nGracias por registrarte en aXXyss Soluciones.\n\nPara activar tu cuenta haz clic en el siguiente enlace:\n\n%(url)s\n\nSi no te has registrado, ignora este mensaje.\n\nUn saludo,\nJoaquin Denis\naXXyss Soluciones\nhttps://axxyss.com\n')) % {'username': user.username, 'url': activation_url},
+                message=message,
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[user.email],
                 fail_silently=True,
