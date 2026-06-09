521a8287b75c14a4b9d51210cbe275238d936f3e
Joaquin <axxyss@gmail.com>
2026-06-09 19:26:28 +0200

    Fix sitemaps protocol and implementation of dynamic localized slugs

--- webpersonal/urls_prod.py
+++ webpersonal/urls_prod.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns, set_language
-from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps.views import index, sitemap
 from core.sitemaps import StaticViewSitemap
 from blog.sitemaps import PostSitemap
 from portfolio.sitemaps import ProjectSitemap
