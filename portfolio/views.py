fa6a42dcc43d868bbaf9ee7d9465b75d7251902e
Joaquin <axxyss@gmail.com>
2026-06-12 11:31:29 +0200

    fix: 301 redirect cuando slug no corresponde al idioma activo

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from .models import Project, ProjectTranslation
 from django.utils.translation import get_language
 
@@ -23,8 +24,24 @@ def project_detail(request, slug):
         project = translation.project
         project.translation = translation
     else:
-        project = get_object_or_404(Project, slug=slug)
-        project.translation = project.translations.filter(language=language_code).first()
+        # Buscar si el slug pertenece a OTRO idioma o al slug base
+        other_translation = ProjectTranslation.objects.filter(
+            slug=slug
+        ).select_related('project').first()
+
+        if other_translation:
+            project = other_translation.project
+        else:
+            project = get_object_or_404(Project, slug=slug)
+
+        # Buscar el slug correcto para el idioma activo
+        correct_translation = project.translations.filter(language=language_code).first()
+        if correct_translation and correct_translation.slug:
+            correct_url = reverse('portfolio:project_detail', kwargs={'slug': correct_translation.slug})
+            return redirect(correct_url, permanent=True)
+        
+        # Si no hay traducción para este idioma, usar el slug base
+        project.translation = None
 
     # Slugs por idioma para el selector
     language_slugs = {'es': project.slug}
